d388b4f0c3401fded752250879b6027116895aa9
Logan Watkins <lw2014wa@gmail.com>
2024-03-20 10:03:09 -0500

    Add htmlnode.py

---
+++ src/htmlnode.py
@@ -0,0 +1,19 @@
+class HTMLNode():
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NonImplementedError()
+
+
+    def props_to_html(self):
+        res = []
+        for k,v in self.props.items():
+            res.append(f" {k}={v}")
+        return res
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
