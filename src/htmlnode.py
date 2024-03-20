e68d88af14b121d8586676c69d05bb120a975986
Logan Watkins <lw2014wa@gmail.com>
2024-03-20 10:49:14 -0500

    Add LeafNode test cases to test_htmlnode.py

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -17,3 +17,17 @@ class HTMLNode():
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("Invalid HTML: no value")
+        if not self.tag:
+            return f"{self.value}"
+        return f"<{self.tag}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
